21e4a1a6cbeb6ac57cfa7f8ec7204f56d54b0c0d
LvndnB <150924843+LvndnB@users.noreply.github.com>
2024-06-10 15:15:34 +0200

    getPitch() methode is toegevoegd

--- Zumo v2 in oop/ZumoIMU.cpp
+++ Zumo v2 in oop/ZumoIMU.cpp
@@ -1,6 +1,11 @@
 #include "ZumoIMU.h"
+#include <cmath>
 
-ZumoIMU::ZumoIMU() {}
+ZumoIMU::ZumoIMU() {
+    float ax = imu.a.x;
+    float ay = imu.a.y;
+    float az = imu.a.z;
+}
 
 void ZumoIMU::init() {
     Wire.begin(); // Nodig voor het gebruik van de library functions
@@ -11,3 +16,12 @@ void ZumoIMU::read() {
     imu.read(); // Uitlezen van de imu sensor
 }
 
+void ZumoIMU::printData(){
+    Serial.println(pitch); // Print de data van de pitch voor het testen
+}
+
+float ZumoIMU::getPitch() {
+    // float pitch = atan2(ax / sqrt(ay * ay + az * az)) * (180 / PI);
+    float pitch = atan2(ax / ay) * (180 / M_PI); // Bron voor de berekening van de pitch: https://atadiat.com/en/e-towards-understanding-imu-basics-of-accelerometer-and-gyroscope-sensors/
+
+}
--- Zumo v2 in oop/ZumoIMU.h
+++ Zumo v2 in oop/ZumoIMU.h
@@ -1,8 +1,11 @@
 #ifndef ZUMO_IMU_H
 #define ZUMO_IMU_H
+#define _USE_MATH_DEFINES
 
 #include <Wire.h>
 #include <Zumo32U4.h>
+
+Zumo32U4IMU imu;
 
 class ZumoIMU {
 public:
@@ -10,9 +13,16 @@ public:
     void init();
     void read();
     void printData();
-    vector<int16_t> m = {0, 0, 0}
+    float getPitch();
+
+    
 
 private:
+    vector<int16_t> a = {0, 0, 0}
+    float ax;
+    float ay;
+    float az;
+    float pitch;
 };
 
 #endif
