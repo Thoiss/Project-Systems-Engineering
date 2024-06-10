86d3fc1036513fa953ed31a2cceb2bc8e943fa9b
Vissieboi <134331831+Vissieboi@users.noreply.github.com>
2024-06-10 14:30:48 +0200

    Update ZumoProxsensor.cpp

---
+++ Zumo v2 in oop/ZumoProxsensor.cpp
@@ -0,0 +1,32 @@
+#include "ZumoProxsensor.h"
+
+ZumoProxsensor::ZumoProxsensor() {}
+
+void ZumoProxsensor::init() {
+    proxSensors.initFrontsensor();
+}
+
+int ZumoProxsensor::read() {
+    proxSensors.read();
+    int frontproxreading = proxSensors.countsFrontWithLeftLeds() + proxSensors.countsFrontWithRightLeds();
+    return frontproxreading;
+}
+
+bool ZumoProxsensor::detect() {
+    if (frontproxreading > 6) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+int ZumoProxsensor::getsensor() {
+    if (detect() = false) {
+        motor.setSpeeds(-50, 50);
+        delay(250);
+    }
+    else {
+        motor.setSpeeds(100, 100);
+    }
+}
